ecb723ab74c0921c6af23fe2fe7a44a3b0ca1c1e
jungsoekoh <63844281+jungsoekoh@users.noreply.github.com>
2021-05-28 17:32:54 +0900

    Detail Page 작성 중...

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var bodyParser = require('body-parser')
 var router = express.Router();
 var fs = require('fs')
 
@@ -7,30 +6,27 @@ var fs = require('fs')
 storagePath = __dirname + '\\' + '..' + '\\' + 'storage' + '\\' + 'todo-json.json'
 
 router.get('/', (req, res, next) => {
-  const todoString = fs.readFileSync(storagePath, 'utf8', function(error, data) {})
-  const todoJSON = JSON.parse(todoString)
+  const todoList = JSON.parse(fs.readFileSync(storagePath, 'utf8'))
 
-  res.status(200).send(todoJSON)
+  res.status(200).send(todoList)
 });
 
 router.post('/', (req, res, next) => {
-  const todoList = JSON.parse(fs.readFileSync(storagePath, 'utf8', function(error, data) {}))
+  const todoList = JSON.parse(fs.readFileSync(storagePath, 'utf8'))
   item = req.body
   todoList.push(item)
   let itemJSON = JSON.stringify(todoList)
   const writeFile = fs.writeFileSync(storagePath, itemJSON)
   
-  console.log(writeFile)
-
   todoList.push(writeFile)
 
   res.status(200).send({success: true})
 })
 
 router.put('/:id', (req, res, next) => {
-  const todoList = JSON.parse(fs.readFileSync(storagePath, 'utf8', function(error, data) {}))
+  const todoList = JSON.parse(fs.readFileSync(storagePath, 'utf8'))
   const renameItem = req.body
-  const targetId = req.body.id
+  const targetId = req.params.id
 
   for(let i = 0; i < todoList.length; i++) {
     if(todoList[i].id == targetId) {
@@ -39,12 +35,11 @@ router.put('/:id', (req, res, next) => {
   }
   let itemJSON = JSON.stringify(todoList)
   const changedFile = fs.writeFileSync(storagePath, itemJSON)
-  console.log(changedFile)
   res.status(200).send({success: true})
 })
 
 router.delete('/:id', (req, res, next) => {
-  const todoList = JSON.parse(fs.readFileSync(storagePath, 'utf8', function(error, data) {}))
+  const todoList = JSON.parse(fs.readFileSync(storagePath, 'utf8'))
   const targetId = req.params.id
 
   for(let i = 0; i < todoList.length; i++) {
@@ -54,10 +49,20 @@ router.delete('/:id', (req, res, next) => {
   }
   let itemJSON = JSON.stringify(todoList)
   const deletedFile = fs.writeFileSync(storagePath, itemJSON)
-  console.log(deletedFile)
   res.status(200).send({success: true})
 })
 
+router.get('/detail/:id', (req, res, next) => {
+  const todoList = JSON.parse(fs.readFileSync(storagePath, 'utf8'))
+  const targetId = req.params.id
+
+  for(let i = 0; i < todoList.length; i++) {
+    if(todoList[i].id == targetId) {
+      // id 중복조회시 Cannot set headers after they are sent to the client 에러 발생
+      res.status(200).send(todoList[i])
+    }
+  }
+});
 
 
 module.exports = router;
